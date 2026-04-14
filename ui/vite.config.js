ec44ef07f828e7af80ab2aa530b8e39fd65df68e
minghui.liu <minghui.liu@ninebot.com>
2026-04-14 09:59:37 +0800

    feat: configure Vite SCSS preprocessing with alias and modern compiler

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/variables.scss" as *; @use "@/styles/mixins.scss" as *;`,
+        api: 'modern-compiler'
+      }
+    }
+  },
   server: {
     port: 3000,
     proxy: {
